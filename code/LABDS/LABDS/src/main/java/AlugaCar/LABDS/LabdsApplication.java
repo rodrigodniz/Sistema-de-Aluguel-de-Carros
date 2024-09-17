36c4c7fe42d6b75e4b4d085570ce920ab4f4e6d0
Rodrigo <dinizcarvalho177@gamil.com>
2024-09-16 21:28:35 -0300

    add: Criação da estrutura do projeto

---
+++ code/LABDS/LABDS/src/main/java/AlugaCar/LABDS/LabdsApplication.java
@@ -0,0 +1,13 @@
+package AlugaCar.LABDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabdsApplication.class, args);
+	}
+
+}
---
+++ code/LABDS/LABDS/src/test/java/AlugaCar/LABDS/LabdsApplicationTests.java
@@ -0,0 +1,13 @@
+package AlugaCar.LABDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
